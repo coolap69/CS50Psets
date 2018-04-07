7a10555d35a166381c4e31fdc7277f7f8401eb19
Louis Pazos <louispazos@gmail.com>
2018-04-07 03:55:11 +0000

    Hello is done, added water

---
+++ water.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+{
+int minutes = get_int();
+}
+
+
+//task
+//converting minutes to bottles of water
+
+//step 1
+////prompt (how many minutes they shower?)
+
+//step 2
+//validte user input (answer)
+
+//step 3
+//calculate equivalent bottles
+//12 bottles = 1 minute
+//printf = answer
+
+//print equivalent bottles
