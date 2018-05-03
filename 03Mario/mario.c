1c68dd60389bc9bf9c6c5c3753c992494d3d5cca
Louis Pazos <coolap69@aol.com>
2018-05-03 19:34:18 +0000

    hello world

--- 03Mario/mario.c
+++ 03Mario/mario.c
@@ -2,14 +2,37 @@
 #include <stdio.h>
 
 int main(void)
-{
-    //initial declaring of variables
-    int minutes;
-    int bottles = 0;
 
-    // prompt user for minutes and verify it is valid number
+    int n = get_int("height: "")
 
+    printf("#\n");
 }
+
+// while (conditions)
+// {
+    //something happens
+// }
+
+// do-while loops
+// execute the body at least once
+
+// int n;
+// do
+// {
+//     n =get_int();
+// }
+// while(condition);
 
 //prompt and validate user input
 //draw the half pyramid
+
+// Valid heights: [0,23]
+// invalid heights: less than 0, more than 23
+
+// get_int
+// SC50 Library function
+// ensure that the user inputs an integer
+// positive integers
+// negative integers
+// 0
+// otherwise, user is prompted to "Retry:"
